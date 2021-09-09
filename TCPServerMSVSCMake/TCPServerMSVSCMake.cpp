72901ec2225a12caa188670b51489e53aa3774dd
Daniil Khaziev <dr.hazdan1999@gmail.com>
2021-09-09 23:39:36 +0500

    Добавьте файлы проекта.

---
+++ TCPServerMSVSCMake/TCPServerMSVSCMake.cpp
@@ -0,0 +1,52 @@
+﻿// TCPServerMSVSCMake.cpp: определяет точку входа для приложения.
+//
+
+#include "TCPServerMSVSCMake.hpp"
+
+
+using namespace std;
+
+Server::Server(uint16_t port) : port(port){
+	
+	uv_tcp_init(loop, &serv);
+
+	uv_ip4_addr("127.0.0.1", port, &addr);
+	uv_tcp_bind(&serv, (const struct sockaddr*)&addr, 0);
+	ret = uv_listen((uv_stream_t*)&serv, NULL, NULL);
+    
+}
+
+Server::~Server()
+{
+
+}
+
+void Server::handlingLoop()
+{
+
+}
+
+void Server::on_accept(uv_tcp_t* handler) {
+	assert(handler == &serv);
+	printf("on conection\n");
+
+	uv_tcp_t* client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+	int r = uv_accept((uv_stream_t*)&serv, (uv_stream_t*)&client);
+	//uv_fs_t* fs;
+
+	if (r) {
+		//int err = uv_fs_get_system_error(fs);
+		//fprintf(stderr, "error accept: %s\n", uv_strerror(err));
+		return;
+	}
+}
+
+int main(uint16_t host, uv_loop_t* loop) {
+	
+	uv_loop_init(loop);
+	Server(8000);
+	
+	uv_run(loop, UV_RUN_DEFAULT);
+
+    return 0;
+}
---
+++ TCPServerMSVSCMake/TCPServerMSVSCMake.hpp
@@ -0,0 +1,39 @@
+﻿#pragma once
+#ifndef TCPSERVERMVSCMAKE_H
+#define TCPSEVERMSVSCMAKE_H
+
+#include <iostream>
+#include <cassert>
+#include "libs/includes/uv.h"
+
+
+class Server {
+public:
+
+	Server(uint16_t port);
+	~Server();
+	uv_loop_t* loop;
+	uv_tcp_t serv;
+	void handlingLoop();
+	
+	void on_accept(uv_tcp_t* handler);
+	
+	
+private:
+
+	uv_thread_t thread_handler;
+	uint16_t port;
+	
+	struct sockaddr_in addr;
+	char* buffer;
+	int socket;
+	int ret;
+	
+
+protected:
+	uv_alloc_cb alloc_buffer_cb;
+	uv_read_cb socket_read_cb;
+};
+
+#endif // !TCPSERVERMVSCMAKE_H
+
