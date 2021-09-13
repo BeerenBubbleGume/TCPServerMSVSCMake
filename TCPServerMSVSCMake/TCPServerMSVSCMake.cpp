0214c1afd1266d623d6057efb82e016cd6f0f2fe
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2021-09-13 10:02:22 +0300

    отказался от ООП

--- TCPServerMSVSCMake/TCPServerMSVSCMake.cpp
+++ TCPServerMSVSCMake/TCPServerMSVSCMake.cpp
@@ -6,47 +6,33 @@
 
 using namespace std;
 
-Server::Server(uint16_t port) : port(port){
-	
+uv_loop_t* loop = uv_default_loop();
+uv_tcp_t serv;
+
+int main() {
+
+
 	uv_tcp_init(loop, &serv);
-
-	uv_ip4_addr("127.0.0.1", port, &addr);
+	struct sockaddr_in addr;
+	uv_ip4_addr("0.0.0.0", 8000, &addr);
 	uv_tcp_bind(&serv, (const struct sockaddr*)&addr, 0);
-	ret = uv_listen((uv_stream_t*)&serv, NULL, NULL);
-    
-}
 
-Server::~Server()
-{
-
-}
-
-void Server::handlingLoop()
-{
-
-}
-
-void Server::on_accept(uv_tcp_t* handler) {
-	assert(handler == &serv);
-	printf("on conection\n");
-
-	uv_tcp_t* client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
-	int r = uv_accept((uv_stream_t*)&serv, (uv_stream_t*)&client);
-	//uv_fs_t* fs;
-
-	if (r) {
-		//int err = uv_fs_get_system_error(fs);
-		//fprintf(stderr, "error accept: %s\n", uv_strerror(err));
-		return;
-	}
-}
-
-int main(uint16_t host, uv_loop_t* loop) {
-
-	uv_loop_init(loop);
-	Server(8000);
+	int ret = uv_listen((uv_stream_t*)&serv, 128, NULL);
 
 	uv_run(loop, UV_RUN_DEFAULT);
+}
 
-	return 0;
+void handlingLoop()
+{
+
+}
+
+void on_accept(uv_tcp_t* handler)
+{
+
+}
+
+void on_close(uv_tcp_t* handler)
+{
+	
 }
--- TCPServerMSVSCMake/TCPServerMSVSCMake.h
+++ TCPServerMSVSCMake/TCPServerMSVSCMake.h
@@ -5,30 +5,11 @@
 #include <cassert>
 #include "libs/includes/uv.h"
 
-class Server {
-public:
-
-	Server(uint16_t port);
-	~Server();
-	uv_loop_t* loop;
-	uv_tcp_t serv;
-	void handlingLoop();
-	
-	void on_accept(uv_tcp_t* handler);	
-	
-private:
-
-	uv_thread_t thread_handler;
-	uint16_t port;
-	struct sockaddr_in addr;
-	char* buffer;
-	int socket;
-	int ret;
-	
-protected:
-	uv_alloc_cb alloc_buffer_cb;
-	uv_read_cb socket_read_cb;
-};
-
+uv_thread_t thread_handler;
+uint16_t port;
+char* buffer;
+void handlingLoop();
+void on_accept(uv_tcp_t* handler);
+void on_close(uv_tcp_t* handler);
 #endif // !TCPSERVERMVSCMAKE_H
 
