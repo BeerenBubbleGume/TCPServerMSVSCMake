b8cbb71bf0de55ab6a22c559b7695d499fc063a9
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2021-09-13 12:00:14 +0300

    on_accept(uv_stream_t* handler, int status)

--- TCPServerMSVSCMake/TCPServerMSVSCMake.h
+++ TCPServerMSVSCMake/TCPServerMSVSCMake.h
@@ -7,9 +7,11 @@
 
 uv_thread_t thread_handler;
 uint16_t port;
+uv_alloc_cb alloc_buffer;
+uv_read_cb read_buffer;
 char* buffer;
 void handlingLoop();
-void on_accept(uv_tcp_t* handler);
+void on_accept(uv_stream_t* handler, int status);
 void on_close(uv_tcp_t* handler);
 #endif // !TCPSERVERMVSCMAKE_H
 
