9de280e36c30806516fe77a7c0cd0ee669efe879
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-02-15 15:08:53 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.hpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.hpp
@@ -96,80 +96,81 @@ protected:
 public:
 
 	static void GenerateRTSPURL(void* Data) {
-		Server::RTSPProxyServer::StartProxyServer(Data);
+		RTSPProxyServer::StartProxyServer(Data);
 		return;
 	}
 
-	class RTSPProxyServer : public RTSPServer
-	{
-	public:
-		static RTSPProxyServer* createNew(UsageEnvironment& env, Port ourPort = 554,
-			UserAuthenticationDatabase* authDatabase = NULL,
-			unsigned reclamationSeconds = 65);
-
-		static void anonceStream(RTSPServer* rtspServer, ServerMediaSession* sms, char const* streamName);
-
-		static void StartProxyServer(/*CString* inputURL, */void* Data);
-		bool StopProxyServer(void* clientData);
-		int getSocket4() { return fServerSocketIPv4; }
-		int getSocket6() { return fServerSocketIPv6; }
-		static void ip4SocketHandler(void* data, int mask) {
-			RTSPProxyServer* server = (RTSPProxyServer*)data;
-			server->incomingConnectionHandlerIPv4();
-		}
-		static void ip6SocketHandler(void* data, int mask) {
-			RTSPProxyServer* server = (RTSPProxyServer*)data;
-			server->incomingConnectionHandlerIPv6();
-		}
-		void resetLoopWatchVaraible(volatile char eventLoopWatchVariable) { this->eventLoopWatchVariable = eventLoopWatchVariable; }
-	protected:
-		virtual ~RTSPProxyServer();
-		RTSPProxyServer(UsageEnvironment& env,
-			int ourSocketIPv4, int ourSocketIPv6, Port ourPort,
-			UserAuthenticationDatabase* authDatabase,
-			unsigned reclamationSeconds);
-		friend class DemandServerMediaSubsession;
-		friend class NetBuffer;
-		volatile char eventLoopWatchVariable;
-	};
-
 protected:
-	class DemandServerMediaSubsession : public OnDemandServerMediaSubsession
-	{
-	public:
-		static DemandServerMediaSubsession* createNew(/*Net* net, */UsageEnvironment& env, Boolean reuseFirstSource);
-		virtual ~DemandServerMediaSubsession();
-		static ServerMediaSession* createNewSMS(UsageEnvironment& env, const char* fileName, FILE* fid);
-		virtual char const* sdpLines(int addressFamily) { return OnDemandServerMediaSubsession::sdpLines(addressFamily); }
-		
-		//void pauseStream1(unsigned clientID, void* streamToken);
-		//virtual void pause();
+	
 
-		/*virtual void SetupSinkAndSource(RTPSink* sink, BasicUDPSink* udp, FramedSource* source) {
-			this->fUDPSink = udp;
-			this->fRTPSink = sink;
-			this->fMediaSource = source;
-		}*/
-	protected:
-		virtual FramedSource* createNewStreamSource(unsigned clientSessionId, unsigned& estBitrate);
-		virtual RTPSink* createNewRTPSink(Groupsock* rtpGroupsock, unsigned char rtpPayloadTypeIfDynamic, FramedSource* inputSource);
-	private:
+};
 
-		DemandServerMediaSubsession(/*Net* net, */UsageEnvironment& env, Boolean reuseFirstSource);
-		static void subsessionByeHandler(void* clientData);
-		void subsessionByeHandler();
+class RTSPProxyServer : public RTSPServer
+{
+public:
+	static RTSPProxyServer* createNew(UsageEnvironment& env, Port ourPort = 554,
+		UserAuthenticationDatabase* authDatabase = NULL,
+		unsigned reclamationSeconds = 65);
 
-		//FramedSource* fMediaSource;
-		//RTPSink* fRTPSink;
-		//BasicUDPSink* fUDPSink;
-		//Boolean fAreCurrentlyPlaying;
-		//Boolean fReuseFirstSource;
-		u_int8_t* fBuffer;
-		u_int64_t fBufferSize;
-		friend class RTSPProxyServer;
-		/*Net* net;*/
-	};
+	static void anonceStream(RTSPServer* rtspServer, ServerMediaSession* sms, char const* streamName);
 
+	static void StartProxyServer(/*CString* inputURL, */void* Data);
+	bool StopProxyServer(void* clientData);
+	int getSocket4() { return fServerSocketIPv4; }
+	int getSocket6() { return fServerSocketIPv6; }
+	static void ip4SocketHandler(void* data, int mask) {
+		RTSPProxyServer* server = (RTSPProxyServer*)data;
+		server->incomingConnectionHandlerIPv4();
+	}
+	static void ip6SocketHandler(void* data, int mask) {
+		RTSPProxyServer* server = (RTSPProxyServer*)data;
+		server->incomingConnectionHandlerIPv6();
+	}
+	void resetLoopWatchVaraible(volatile char eventLoopWatchVariable) { this->eventLoopWatchVariable = eventLoopWatchVariable; }
+protected:
+	virtual ~RTSPProxyServer();
+	RTSPProxyServer(UsageEnvironment& env,
+		int ourSocketIPv4, int ourSocketIPv6, Port ourPort,
+		UserAuthenticationDatabase* authDatabase,
+		unsigned reclamationSeconds);
+	friend class DemandServerMediaSubsession;
+	volatile char eventLoopWatchVariable;
+};
+
+class DemandServerMediaSubsession : public OnDemandServerMediaSubsession
+{
+public:
+	static DemandServerMediaSubsession* createNew(/*Net* net, */UsageEnvironment& env, Boolean reuseFirstSource);
+	virtual ~DemandServerMediaSubsession();
+	static ServerMediaSession* createNewSMS(UsageEnvironment& env, const char* fileName, FILE* fid);
+	virtual char const* sdpLines(int addressFamily) { return OnDemandServerMediaSubsession::sdpLines(addressFamily); }
+
+	//void pauseStream1(unsigned clientID, void* streamToken);
+	//virtual void pause();
+
+	/*virtual void SetupSinkAndSource(RTPSink* sink, BasicUDPSink* udp, FramedSource* source) {
+		this->fUDPSink = udp;
+		this->fRTPSink = sink;
+		this->fMediaSource = source;
+	}*/
+protected:
+	virtual FramedSource* createNewStreamSource(unsigned clientSessionId, unsigned& estBitrate);
+	virtual RTPSink* createNewRTPSink(Groupsock* rtpGroupsock, unsigned char rtpPayloadTypeIfDynamic, FramedSource* inputSource);
+private:
+
+	DemandServerMediaSubsession(/*Net* net, */UsageEnvironment& env, Boolean reuseFirstSource);
+	static void subsessionByeHandler(void* clientData);
+	void subsessionByeHandler();
+
+	//FramedSource* fMediaSource;
+	//RTPSink* fRTPSink;
+	//BasicUDPSink* fUDPSink;
+	//Boolean fAreCurrentlyPlaying;
+	//Boolean fReuseFirstSource;
+	u_int8_t* fBuffer;
+	u_int64_t fBufferSize;
+	friend class RTSPProxyServer;
+	/*Net* net;*/
 };
 
 #endif
