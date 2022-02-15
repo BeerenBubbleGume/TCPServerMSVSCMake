b1a05192e3c289cd19265952501f947e722f14d8
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-02-15 15:07:52 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
@@ -380,7 +380,7 @@ void serverBYEHandler(void* instance, u_int8_t requestBytes)
 
 void proxyServerMediaSubsessionAfterPlaying(void* clientData)
 {
-	Server::RTSPProxyServer* server = (Server::RTSPProxyServer*)clientData;
+	RTSPProxyServer* server = (RTSPProxyServer*)clientData;
 	MediaSubsession* subsession = (MediaSubsession*)clientData;
 	NetSocketUV* uvsocket = (NetSocketUV*)GetNetSocketPtr(clientData);
 	volatile char eventLoopWatchVariable = 1;
@@ -419,7 +419,7 @@ int Server::connect(bool connection)
 		if (net_sockuv->Create(8554, true, true) == true)
 		{
 			std::cout << "receiving success, start retranslate" << std::endl;
-			Server::RTSPProxyServer::StartProxyServer(net_sockuv);
+			RTSPProxyServer::StartProxyServer(net_sockuv);
 		}
 		return 0;
 	}
@@ -430,17 +430,17 @@ int Server::connect(bool connection)
 	}
 }
 
-Server::RTSPProxyServer* Server::RTSPProxyServer::createNew(UsageEnvironment& env, Port ourPort, UserAuthenticationDatabase* authDatabase, unsigned reclamationSeconds)
+RTSPProxyServer* RTSPProxyServer::createNew(UsageEnvironment& env, Port ourPort, UserAuthenticationDatabase* authDatabase, unsigned reclamationSeconds)
 {
 	int ourSocket4 = setUpOurSocket(env, ourPort, AF_INET);
 	if (ourSocket4 == -1) return nullptr;
 	int ourSocket6 = setUpOurSocket(env, ourPort, AF_INET6);
 	if (ourSocket6 == -1) return nullptr;
-	return new Server::RTSPProxyServer(env, ourSocket4, ourSocket6, ourPort, authDatabase, reclamationSeconds);
+	return new RTSPProxyServer(env, ourSocket4, ourSocket6, ourPort, authDatabase, reclamationSeconds);
 }
 
 
-void Server::RTSPProxyServer::anonceStream(RTSPServer* rtspServer, ServerMediaSession* sms, char const* streamName)
+void RTSPProxyServer::anonceStream(RTSPServer* rtspServer, ServerMediaSession* sms, char const* streamName)
 {
 	char* url = rtspServer->rtspURL(sms);
 	UsageEnvironment& env = rtspServer->envir();
@@ -449,7 +449,7 @@ void Server::RTSPProxyServer::anonceStream(RTSPServer* rtspServer, ServerMediaSe
 	delete[] url;
 }
 
-void Server::RTSPProxyServer::StartProxyServer(/*CString* inputURL, */void* Data)
+void RTSPProxyServer::StartProxyServer(/*CString* inputURL, */void* Data)
 {
 	TaskScheduler* newscheduler = BasicTaskScheduler::createNew();
 	UsageEnvironment* env = BasicUsageEnvironment::createNew(*newscheduler);
@@ -486,7 +486,7 @@ void Server::RTSPProxyServer::StartProxyServer(/*CString* inputURL, */void* Data
 	const char* streamName = "ServerMedia/"/* + *socket->GetClientID()*/;
 
 	ServerMediaSession* sms = ServerMediaSession::createNew(*env, streamName);
-	DemandServerMediaSubsession* proxy = Server::DemandServerMediaSubsession::createNew(/*socket->net, */*env, true);
+	DemandServerMediaSubsession* proxy = DemandServerMediaSubsession::createNew(/*socket->net, */*env, true);
 
 	//BasicUDPSink* UDPsink = BasicUDPSink::createNew(*env, rtpGS);
 	//StreamState* state = new StreamState(*proxy, server->fServerPort, server->fServerPort, outputSink, UDPsink, estimatedSessionBandwidth, outSource, rtpGS, rtcpGS);
@@ -509,16 +509,16 @@ void Server::RTSPProxyServer::StartProxyServer(/*CString* inputURL, */void* Data
 	//return;
 }
 
-bool Server::RTSPProxyServer::StopProxyServer(void* clientData)
+bool RTSPProxyServer::StopProxyServer(void* clientData)
 {
 	return false;
 }
 
-Server::RTSPProxyServer::~RTSPProxyServer()
+RTSPProxyServer::~RTSPProxyServer()
 {
 }
 
-Server::RTSPProxyServer::RTSPProxyServer(UsageEnvironment& env,
+RTSPProxyServer::RTSPProxyServer(UsageEnvironment& env,
 	int ourSocketIPv4, int ourSocketIPv6, Port ourPort,
 	UserAuthenticationDatabase* authDatabase,
 	unsigned reclamationSeconds)
@@ -528,12 +528,12 @@ Server::RTSPProxyServer::RTSPProxyServer(UsageEnvironment& env,
 }
 
 
-Server::DemandServerMediaSubsession* Server::DemandServerMediaSubsession::createNew(/*Net* net, */UsageEnvironment& env, Boolean reuseFirstSource)
+DemandServerMediaSubsession* DemandServerMediaSubsession::createNew(/*Net* net, */UsageEnvironment& env, Boolean reuseFirstSource)
 {
 	return new DemandServerMediaSubsession(/*net, */env, reuseFirstSource);
 }
 
-Server::DemandServerMediaSubsession::~DemandServerMediaSubsession()
+DemandServerMediaSubsession::~DemandServerMediaSubsession()
 {
 	if (fBuffer)
 	{
@@ -549,7 +549,7 @@ const char* destStr = description\
 sms = ServerMediaSession::createNew(env, fileName, fileName, destStr);\
 }while(0)
 
-ServerMediaSession* Server::DemandServerMediaSubsession::createNewSMS(UsageEnvironment& env, const char* fileName, FILE* fid)
+ServerMediaSession* DemandServerMediaSubsession::createNewSMS(UsageEnvironment& env, const char* fileName, FILE* fid)
 {
 	const char* extension = strrchr(fileName, '.');
 	if (extension == nullptr) return NULL;
@@ -567,7 +567,7 @@ ServerMediaSession* Server::DemandServerMediaSubsession::createNewSMS(UsageEnvir
 	return sms;
 }
 
-FramedSource* Server::DemandServerMediaSubsession::createNewStreamSource(unsigned clientSessionId, unsigned& estBitrate)
+FramedSource* DemandServerMediaSubsession::createNewStreamSource(unsigned clientSessionId, unsigned& estBitrate)
 {
 	/*NetBuffer* recvBuff = net->GetRecvBuffer();
 	fBuffer = recvBuff->GetData();
@@ -583,7 +583,7 @@ FramedSource* Server::DemandServerMediaSubsession::createNewStreamSource(unsigne
 	return H264VideoStreamFramer::createNew(envir(), fileSource);
 }
 
-RTPSink* Server::DemandServerMediaSubsession::createNewRTPSink(Groupsock* rtpGroupsock, unsigned char rtpPayloadTypeIfDynamic, FramedSource* inputSource)
+RTPSink* DemandServerMediaSubsession::createNewRTPSink(Groupsock* rtpGroupsock, unsigned char rtpPayloadTypeIfDynamic, FramedSource* inputSource)
 {
 	return SimpleRTPSink::createNew(envir(), rtpGroupsock, rtpPayloadTypeIfDynamic, 9000, "video", "H264", 1, True, True);
 }
@@ -604,15 +604,15 @@ RTPSink* Server::DemandServerMediaSubsession::createNewRTPSink(Groupsock* rtpGro
 //	fAreCurrentlyPlaying = False;
 //}
 
-void Server::DemandServerMediaSubsession::subsessionByeHandler(void* clientData)
+void DemandServerMediaSubsession::subsessionByeHandler(void* clientData)
 {
 }
 
-void Server::DemandServerMediaSubsession::subsessionByeHandler()
+void DemandServerMediaSubsession::subsessionByeHandler()
 {
 }
 
-Server::DemandServerMediaSubsession::DemandServerMediaSubsession(/*Net* net, */UsageEnvironment& env, Boolean reuseFirstSource) : OnDemandServerMediaSubsession(env, reuseFirstSource)/*, net(net)*/
+DemandServerMediaSubsession::DemandServerMediaSubsession(/*Net* net, */UsageEnvironment& env, Boolean reuseFirstSource) : OnDemandServerMediaSubsession(env, reuseFirstSource)/*, net(net)*/
 {
 	fBufferSize = 100000000;
 	fBuffer = new u_int8_t[fBufferSize];
