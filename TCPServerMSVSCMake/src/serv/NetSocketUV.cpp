fa3019cd91e3aa8ab574a281e9810e2a6e0868a5
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-03-16 17:30:06 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
@@ -321,139 +321,6 @@ void NetSocketUV::Destroy()
 	NetSocket::Destroy();
 }
 
-//void NetSocketUV::decode(AVCodecContext* dec_cont, AVFrame* frame, AVPacket* packet, const char* fileName)
-//{
-//	char buf[10240];
-//	int ret;
-//
-//	ret = avcodec_send_packet(dec_cont, packet);
-//	if (ret < 0)
-//	{
-//		fprintf(stderr, "Error sending a packet for decoding!\n");
-//		exit(1);
-//	}
-//	while (ret >= 0) {
-//		ret = avcodec_receive_frame(dec_cont, frame);
-//		if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-//			return;
-//		else if (ret < 0) 
-//		{
-//			fprintf(stderr, "Error during decoding\n");
-//			exit(1);
-//			
-//		}
-//		
-//		printf("saving frame %3d\n", dec_cont->frame_number);
-//		fflush(stdout);
-//			
-//		snprintf(buf, sizeof(buf), "%s-%d", fileName, dec_cont->frame_number);
-//		pgm_save(frame->data[0], frame->linesize[0], frame->width, frame->height, buf);
-//	}
-//}
-//
-//void NetSocketUV::pgm_save(unsigned char* buf, int wrap, int xsize, int ysize, char* filename)
-//{
-//	FILE* f;
-//	int i;
-//
-//	f = fopen(filename, "ab+");
-//	fprintf(f, "P5\n%d\n%d", xsize, ysize, 255);
-//	for (i = 0; i < ysize; i++)
-//		fwrite(buf + i * wrap, 1, xsize, f);
-//	fclose(f);
-//}
-//
-//void NetSocketUV::setupDecoder(void* Data)
-//{
-//	//NetBuffer* recv_buffer = net->GetRecvBuffer();
-//	const char* fileName, *outFileName;
-//	const AVCodec* codec;
-//	AVCodecParserContext* parser;
-//	AVCodecContext* cont = nullptr;
-//	FILE* file;
-//
-//	AVFrame* frame;
-//	uint8_t inBuff[4096 + AV_INPUT_BUFFER_PADDING_SIZE];
-//	uint8_t* data;
-//	size_t data_size;
-//	int ret;
-//	AVPacket* packet;
-//
-//
-//	fileName = "in_binary_h.264";
-//	outFileName = "out_h.264";
-//
-//	packet = av_packet_alloc();
-//	if (!packet)
-//	{
-//		exit(1);
-//	}
-//	
-//	memset(inBuff + 4096, 0, AV_INPUT_BUFFER_PADDING_SIZE);
-//	codec = avcodec_find_decoder(AV_CODEC_ID_H264);
-//	if (!codec)
-//	{
-//		fprintf(stderr, "Codec not found\n");
-//		exit(1);
-//	}
-//	parser = av_parser_init(codec->id);
-//	if (!parser)
-//	{
-//		fprintf(stderr, "parser not found\n");
-//		exit(1);
-//	}
-//	cont = avcodec_alloc_context3(codec);
-//	if (!cont)
-//	{
-//		fprintf(stderr, "Could not allocate video codec context!\n");
-//		exit(1);
-//	}
-//
-//	if (avcodec_open2(cont, codec, nullptr) < 0)
-//	{
-//		fprintf(stderr, "Could not open codec\n");
-//		exit(1);
-//	}
-//	file = fopen(fileName, "rb");
-//	if (!file)
-//	{
-//		fprintf(stderr, "Could not open %s\n", fileName);
-//		exit(1);
-//	}
-//	frame = av_frame_alloc();
-//	if (!frame)
-//	{
-//		fprintf(stderr, "Could not allocate video frame\n");
-//		exit(1);
-//	}
-//	while (!feof(file))
-//	{
-//		data_size = fread(inBuff, 1, 4096, file);
-//		if (!data_size)
-//			break;
-//		data = inBuff;
-//		while (data_size > 0)
-//		{
-//			ret = av_parser_parse2(parser, cont, &packet->data, &packet->size, data, data_size, AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
-//			if (ret < 0)
-//			{
-//				fprintf(stderr, "Error while parsing\n");
-//				exit(1);
-//			}
-//			data += ret;
-//			data_size -= ret;
-//			if (packet->size)
-//				decode(cont, frame, packet, outFileName);
-//		}
-//	}
-//	decode(cont, frame, nullptr, outFileName);
-//	fclose(file);
-//	av_parser_close(parser);
-//	avcodec_free_context(&cont);
-//	av_frame_free(&frame);
-//	av_packet_free(&packet);
-//}
-
 Server::Server()
 {
 	net = new Net;
@@ -475,7 +342,7 @@ int Server::connect(bool connection)
 		//udp_tcp = true;
 		std::cout << "==========Start server!==========" << std::endl;
 		//net_sockuv->GetIP(NULL, true);
-		if (net_sockuv->Create(8554, true, true) == true)
+		if (net_sockuv->Create(80, true, true) == true)
 			return 0;
 	}
 	else
