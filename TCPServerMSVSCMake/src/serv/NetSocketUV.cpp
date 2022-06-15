9584e46fdd72023d22a2d422c007f0e7b415dc3f
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-06-15 11:01:27 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
@@ -453,7 +453,7 @@ void ServerUV::StartUVServer(bool internet)
 	if (internet)
 	{
 		printf("What a protocol should been used: UDP or TCP?\n");
-		char str = new char[4];
+		char* str = new char[4];
 		CString proto;
 		scanf("%s", str);
 		proto = str;
