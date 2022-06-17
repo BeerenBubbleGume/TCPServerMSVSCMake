b2f66d2778deea85db247e67c0f30ef74e3bd289
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-06-17 10:59:22 +0500

    commit

--- TCPServerMSVSCMake/src/serv/FF_encoder.cpp
+++ TCPServerMSVSCMake/src/serv/FF_encoder.cpp
@@ -1,1 +1,77 @@
 #include "FF_encoder.hpp"
+
+void FF_encoder::encode(AVCodecContext* enc_ctx, AVFrame* frame, AVPacket* pkt, FILE* outFile)
+{
+    int ret;
+
+    if (frame)
+        printf("Send frame %3"PRId64"\n", frame->pts);
+
+    ret = avcodec_send_frame(enc_ctx, frame);
+    if (ret < 0) {
+        fprintf(stderr, "Error sending a frame for encoding\n");
+        exit(1);
+    }
+
+    while (ret >= 0) {
+        ret = avcodec_receive_packet(enc_ctx, pkt);
+        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+            return;
+        else if (ret < 0) {
+            fprintf(stderr, "Error during encoding\n");
+            exit(1);
+        }
+
+        printf("Write packet %3"PRId64" (size=%5d)\n", pkt->pts, pkt->size);
+        fwrite(pkt->data, 1, pkt->size, outfile);
+        av_packet_unref(pkt);
+    }
+}
+
+FF_encoder* FF_encoder::createNew(unsigned char* inBuff, unsigned inBufferSize, const char* outFileName, const char* codecName)
+{
+	return new FF_encoder(inBuff, inBufferSize, outFileName, codecName);
+}
+
+void FF_encoder::Clear()
+{
+}
+
+void FF_encoder::ReadIncommigDataBuff()
+{
+    
+}
+
+FF_encoder::FF_encoder(unsigned char* inBuff, unsigned inBuferSize, const char* outFileName, const char* codecName): fData(inBuff), fDataSize(inBuferSize), 
+fFileName(outFileName), fCodecName(codecName)
+{
+    fContext = nullptr;
+    fFile = nullptr;
+    fFrame = nullptr;
+    fPacket = nullptr;
+    fCodec = avcodec_find_encoder_by_name(fCodecName);
+    if (!fCodec)
+    {
+        fprintf(stderr, "Codec '%s' not found\n", fCodecName);
+        exit(1);
+    }
+
+    fContext = avcodec_alloc_context3(fCodec);
+    if (!fContext)
+    {
+        fprintf(stderr, "Could not allocate video codec context\n");
+        exit(1);
+    }
+
+    fPacket = av_packet_alloc();
+    if (!fPackte)
+    {
+        fprintf(stderr, "Could not allocate packet\n");
+        exit(1);
+    }
+}
+
+FF_encoder::~FF_encoder()
+{
+    Clear();
+}
--- TCPServerMSVSCMake/src/serv/FF_encoder.hpp
+++ TCPServerMSVSCMake/src/serv/FF_encoder.hpp
@@ -1,15 +1,34 @@
+#pragma once
 #include "includes.hpp"
+extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavformat/avio.h>
 #include <libavutil/avutil.h>
-
+#include <libavutil/imgutils.h>
+#include <libavutil/opt.h>
+}
 class FF_encoder
 {
 public:
 
-
+	static void encode(AVCodecContext* enc_ctx, AVFrame* frame, AVPacket* pkt, FILE* outFile);
+	static FF_encoder* createNew(unsigned char* inBuff, unsigned inBufferSize, const char* outFileName, const char* codecName);
+	void Clear();
+	void ReadIncommigDataBuff();
 protected:
+	FF_encoder(unsigned char* inBuff, unsigned inBuferSize, const char* outFileName, const char* codecName);
+	~FF_encoder();
 
-	
+	AVCodecContext* fContext;
+	AVFrame* fFrame;
+	AVPacket* fPacket;
+	FILE* fFile;
+	AVCodec* fCodec;
+
+	unsigned char* fData;
+	unsigned fDataSize;
+
+	const char* fCodecName, *fFileName;
+
 };
