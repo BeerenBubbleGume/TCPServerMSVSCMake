8dbad55353dedd47296eeca91afda0a7d239fd53
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-06-20 15:24:51 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
@@ -168,7 +168,7 @@ void NetSocketUV::ReceiveTCP()
 	fileName.IntToString(ClientID);
 	fileName += "in_binary.h264";
 	
-	FF_encoder* encoder = FF_encoder::createNew(net->GetRecvBuffer()->GetData(), net->GetRecvBuffer()->GetLength(), fileName, "x264");
+	FF_encoder* encoder = FF_encoder::createNew(net->GetRecvBuffer()->GetData(), net->GetRecvBuffer()->GetLength(), fileName, "264");
 	encoder->ReadIncommigDataBuff();
 	
 	/*fout.open(fileName.c_str(), std::ios::binary | std::ios::app);
