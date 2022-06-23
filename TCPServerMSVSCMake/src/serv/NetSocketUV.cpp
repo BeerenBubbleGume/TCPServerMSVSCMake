17157c404e1b813828c677853016d57d943dee7b
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-06-23 11:20:24 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
@@ -134,7 +134,7 @@ bool NetSocketUV::Accept()
 				net->getWR1()->Finish(buf);
 				printf("Accepted client with ID:%u\nIP:\t%s\n", accept_sock->ClientID, accept_sock->ip.c_str());
 				NET_BUFFER_INDEX* result = accept_sock->net->PrepareMessage(accept_sock->ClientID, MESSAGE_TYPE_HELLO, buf.length, buf.data);
-				accept_sock->SendMessage(result);
+				SendMessage(result);
 			/*}
 			else
 			{
