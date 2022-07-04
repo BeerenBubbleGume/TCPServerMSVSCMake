eeba73b68160922ad019723fbff28f53de21b88e
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-07-04 14:30:31 +0500

    commit

--- TCPServerMSVSCMake/src/serv/FF_encoder.cpp
+++ TCPServerMSVSCMake/src/serv/FF_encoder.cpp
@@ -126,7 +126,7 @@ void FF_encoder::SetupOutput()
         fprintf(stderr, "Error occurred when opening output file, %s\n", av_err2str(ret));
     }*/
     av_dump_format(ofmt_ctx, 0, fOutURL, 1);
-end:
+//end:
     //if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
     //    avio_closep(&ofmt_ctx->pb);
     //avformat_free_context(ofmt_ctx);
