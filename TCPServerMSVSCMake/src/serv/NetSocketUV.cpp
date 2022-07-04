614448d205088879bc44f9735c3b563fe965e0f0
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-07-04 15:15:41 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
@@ -166,9 +166,7 @@ bool NetSocketUV::Accept()
 			//FF_encoder* sender = FF_encoder::createNew(accept_sock->ip.c_str(), fileName);
 			/*std::thread RTSPsend(SetupRetranslation, accept_sock, fileName);
 			RTSPsend.detach();*/
-			CString ip("rtsp://");
-			ip += accept_sock->ip;
-			accept_sock->sender = FF_encoder::createNew(ip.c_str());
+			accept_sock->sender = FF_encoder::createNew("rtsp://192.168.0.69:8554/serverPlay/");
 			accept_sock->sender->SetupOutput();
 			printf("Accepted client with ID:%u\nIP:\t%s\nSessionID:\t%u\n\n", accept_sock->ClientID, accept_sock->ip.c_str(), accept_sock->sessionID);
 			
