c4c5b3286f4c5ad5bbadbb444200994a1578d2ee
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-07-05 11:40:26 +0500

    commit

--- TCPServerMSVSCMake/src/serv/FF_encoder.cpp
+++ TCPServerMSVSCMake/src/serv/FF_encoder.cpp
@@ -59,7 +59,7 @@ void FF_encoder::SetupOutput()
 {
     
     options = NULL;
-    ret = av_dict_set(&options, "rtsp_transport", "udp_multicast", 0);
+    ret = av_dict_set(&options, "rtsp_transport", "udp", 0);
     assert(ret >= 0);
     ret = av_dict_set(&options, "announce_addr", "0.0.0.0", 0);
     assert(ret >= 0);
