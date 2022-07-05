70b1e3eabefb49b1539852f54fade4edf679638e
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-07-05 17:25:41 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
@@ -513,7 +513,7 @@ MediaSink* sink = nullptr;
 
 int process_stream(NetSocket* input_sock, bool is_same)
 {
-	if (is_same)
+	if (is_same == false)
 	{
 		TaskScheduler* scheduler = BasicTaskScheduler::createNew();
 		UsageEnvironment* env = BasicUsageEnvironment::createNew(*scheduler);
