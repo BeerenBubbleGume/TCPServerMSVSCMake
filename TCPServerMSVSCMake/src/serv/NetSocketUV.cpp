9c99ee8f92d73d0e831037c2c2bcea0f511b3112
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-07-06 16:41:13 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
@@ -132,7 +132,7 @@ bool NetSocketUV::Accept()
 			bool is_same = false;
 
 			CString fileName;
-			IParr[SessionID] = accept_sock->addr->ip;
+			IParr[sessionID] = accept_sock->addr->ip;
 			if ((is_same = accept_sock->assertIP(IParr, accept_sock->addr->ip)) == true)
 			{
 				printf("assertIP(%p) return true\n");
