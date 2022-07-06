c7dd5a94d4c1685a8a497c5d5073fa6a8e286139
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-07-06 14:44:20 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
@@ -183,7 +183,7 @@ bool NetSocketUV::Accept()
 			/*proc = fork();
 			if (proc == 0)
 			{*/
-				accept_sock->sender = FF_encoder::createNew("rtsp://127.0.0.1:8554/0in_binary.264");
+				accept_sock->sender = FF_encoder::createNew("tcp://127.0.0.1:8554/0in_binary.264");
 				accept_sock->sender->SetupOutput();
 				/*printf("IN CHILED!\n");
 				process_stream(accept_sock, is_same);*/
