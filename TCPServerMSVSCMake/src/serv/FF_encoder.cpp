8ddbfd65dce97858f78bb8a873d1cfb048bd48df
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-07-06 14:26:27 +0500

    commit

--- TCPServerMSVSCMake/src/serv/FF_encoder.cpp
+++ TCPServerMSVSCMake/src/serv/FF_encoder.cpp
@@ -111,7 +111,7 @@ void FF_encoder::SetupOutput()
 
     AVStream* video_track = avformat_new_stream(ofmt_ctx, nullptr);
     //AVStream* audio_track = avformat_new_stream(ofmt_ctx, NULL);
-    ofmt_ctx->oformat->video_codec = AV_CODEC_ID_H264;
+    //ofmt_ctx->oformat->video_codec = AV_CODEC_ID_H264;
     //ofmt_ctx->oformat->audio_codec = AV_CODEC_ID_OPUS;
     
     avformat_network_init();
