0ec1fdb3e969367e09a10b6058b7091d43e7bb9a
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-07-07 13:01:28 +0500

    commit

--- TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
+++ TCPServerMSVSCMake/src/serv/NetSocketUV.cpp
@@ -73,7 +73,7 @@ bool NetSocketUV::Create(int port, bool udp_tcp, bool listen)
 }
 
 char address_converter[30];
-CString *IParr = new CString[10240];
+CString *IParr = {0};
 bool NetSocketUV::GetIP(CString& addr, bool own_or_peer)
 {
 	if (NetSocket::GetIP(addr, own_or_peer))
